e32ffe7ab86afe6bcee778bde33f644d90ff8c96
antoinetran <antoinetran@users.noreply.github.com>
2025-01-17 23:49:03 +0100

    Fix https://github.com/interTwin-eu/interlink-slurm-plugin/issues/48 - adding singularity noeval by default needed for complex env var with JSON value (eg ARGO_TEMPLATE)

--- pkg/slurm/Create.go
+++ pkg/slurm/Create.go
@@ -89,7 +89,8 @@ func (h *SidecarHandler) SubmitHandler(w http.ResponseWriter, r *http.Request) {
 			singularityCommand = "run"
 		}
 
-		commstr1 := []string{"singularity", singularityCommand, "--containall", "--nv", singularityMounts, singularityOptions}
+		// no-eval is important so that singularity does not evaluate env var, because the shellquote has already done the safety check.
+		commstr1 := []string{"singularity", singularityCommand, "--no-eval", "--containall", "--nv", singularityMounts, singularityOptions}
 
 		image := ""
 
